4d4b6ca0b3a82df51940be7c6cd27c783fa85c2f
MlNl-PEKKA <punithg360@gmail.com>
2026-06-03 12:46:48 +0530

    use padding_90

--- include/global.h
+++ include/global.h
@@ -522,15 +522,18 @@ struct SaveBlock2
              u16 optionsBattleStyle:1; // OPTIONS_BATTLE_STYLE_[SHIFT/SET]
              u16 optionsBattleSceneOff:1; // whether battle animations are disabled
              u16 regionMapZoom:1; // whether the map is zoomed in
-             u16 optionsLevelCap_CLASSIC_LEVELCAP:1;
-             u16 optionsForgetHMs_CLASSIC_FORGETHMS:1;
-             u16 optionsInfiniteTMs_CLASSIC_INFINITETMS:1;
-             u16 optionsPermaRepel_CLASSIC_PERMAREPEL:1;
-    /*0x16*/ u16 options1CostItems_CLASSIC_1COSTITEMS:1;
-             u16 optionsEVTraining_CLASSIC_EVTRAINING:2;
-             //u16 padding2: 13 left;
+             //u16 padding1:4;
+             //u16 padding2;
     /*0x18*/ struct Pokedex pokedex;
-    /*0x90*/ u8 filler_90[0x8];
+    /*0x90*/ u8 filler_90[0x6];
+    /*0x96*/ u8 optionsEVTraining_CLASSIC_EVTRAINING:2;
+             u8 optionsLevelCap_CLASSIC_LEVELCAP:1;
+             u8 optionsForgetHMs_CLASSIC_FORGETHMS:1;
+             u8 optionsInfiniteTMs_CLASSIC_INFINITETMS:1;
+             u8 optionsPermaRepel_CLASSIC_PERMAREPEL:1;
+             u8 options1CostItems_CLASSIC_1COSTITEMS:1; 
+             // 1 bit left
+    /*0x97*/ u8 filler_97;
     /*0x98*/ struct Time localTimeOffset;
     /*0xA0*/ struct Time lastBerryTreeUpdate;
     /*0xA8*/ u32 gcnLinkFlags; // Read by Pokémon Colosseum/XD
